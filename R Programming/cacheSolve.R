280961563047986ba918103c0dd93d0c23cd27a1
hbs1 <hbsherm61@gmail.com>
2015-06-18 11:44:34 -0400

    For Programming Assignment2

---
+++ R Programming/cacheSolve.R
@@ -0,0 +1,11 @@
+cacheSolve <- function(x, ...) {
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- inverse(data, ...)
+        x$setinverse(m)
+        m
+}
